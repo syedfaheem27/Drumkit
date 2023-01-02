d5faf4e1a36eb11f6accda00d64187f170adbd7b
Syed Faheem <111534462+syedfaheem27@users.noreply.github.com>
2023-01-02 22:05:08 +0530

    Query the audio element. Queried the audio element having the attribute same as the keycode of the key pressed and also played the audio.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 window.addEventListener('keydown',(e) => {
     const key=document.querySelector(`div[data-key="${e.keyCode}"]`);
     if(!key) return;
+    const audio=document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    audio.play();
+    audio.currentTime=0;
 
 })
